8121d33eb7d5b6953b0bbe14dbac893d2e3d8e49
Rikkimcc <rikkimccall94@gmail.com>
2021-11-11 19:27:59 -0500

    fixed error in append function

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ function buildTable(data) {
     tbody.html("");
 
     data.forEach((dataRow) => {
-        let row = tbody.append.("tr");
+        let row = tbody.append("tr");
         Object.values(dataRow).forEach((val) => {
             let cell = row.append("td");
             cell.text(val);
